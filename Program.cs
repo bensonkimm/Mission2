af1d6010cf48dfd20bec672941d7507545285172
bensonkimm <benson02@byu.edu>
2025-01-15 13:49:34 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the dice rolling Simulator!");
+        Console.WriteLine("How many dice do you want to roll? ");
+
+        if (!int.TryParse(Console.ReadLine(), out int numberofRolls) || numberofRolls <= 0)
+            Console.WriteLine("Invalid input. Please a positive integer.");
+        return;
+    }
+}
